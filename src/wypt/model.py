128495d2fe2287a4589c29fbcdc902296b3fe6b9
Preocts <preocts@gmail.com>
2022-09-02 00:07:23 -0400

    Create models for response data

---
+++ src/wypt/model.py
@@ -0,0 +1,32 @@
+"""Data models for API responses."""
+from __future__ import annotations
+
+import dataclasses
+import json
+
+__all__ = ["Paste"]
+
+
+@dataclasses.dataclass
+class BaseModel:
+    def to_dict(self) -> dict[str, str]:
+        """Convert model to a dictionary."""
+        return dataclasses.asdict(self)
+
+    def to_json(self) -> str:
+        """Convert model to JSON serialized string."""
+        return json.dumps(self.to_dict(), sort_keys=True)
+
+
+@dataclasses.dataclass
+class Paste(BaseModel):
+    scrape_url: str
+    full_url: str
+    date: str
+    key: str
+    size: str
+    expire: str
+    title: str
+    syntax: str
+    user: str
+    hits: str
---
+++ tests/model_test.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+
+import pytest
+from wypt.model import BaseModel
+from wypt.model import Paste
+
+
+@pytest.mark.parametrize(
+    ("fixture", "model"), (("tests/fixture/scrape_resp.json", Paste),)
+)
+def test_model_create_and_deconstruct(fixture: str, model: BaseModel) -> None:
+    resps = json.loads(Path(fixture).read_text())
+
+    for resp in resps:
+        expected_json = json.dumps(resp, sort_keys=True)
+        test_model = model(**resp)  # type: ignore
+
+        test_dict = test_model.to_dict()
+        test_json = test_model.to_json()
+
+        assert test_dict == resp
+        assert test_json == expected_json
