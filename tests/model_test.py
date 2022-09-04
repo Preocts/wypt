422c3111d525287e735ebd7ada4f3630738f0283
Preocts <preocts@gmail.com>
2022-09-04 01:46:08 -0400

    Model paste

--- tests/model_test.py
+++ tests/model_test.py
@@ -2,21 +2,26 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
+from typing import Any
 
 import pytest
-from wypt.model import BaseModel
+from wypt.model import Paste
 from wypt.model import PasteMeta
 
 
 @pytest.mark.parametrize(
-    ("fixture", "model"), (("tests/fixture/scrape_resp.json", PasteMeta),)
+    ("fixture", "model"),
+    (
+        ("tests/fixture/scrape_resp.json", PasteMeta),
+        ("tests/fixture/paste.json", Paste),
+    ),
 )
-def test_model_create_and_deconstruct(fixture: str, model: BaseModel) -> None:
+def test_model_create_and_deconstruct(fixture: str, model: Any) -> None:
     resps = json.loads(Path(fixture).read_text())
 
     for resp in resps:
         expected_json = json.dumps(resp, sort_keys=True)
-        test_model = model(**resp)  # type: ignore
+        test_model = model(**resp)
 
         test_dict = test_model.to_dict()
         test_json = test_model.to_json()
