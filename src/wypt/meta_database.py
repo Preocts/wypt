a52119ddae301b10049487202f7e5df27bb1adc8
Preocts <preocts@gmail.com>
2022-09-05 23:39:36 -0400

    lookup metas that need to be collected

--- src/wypt/meta_database.py
+++ src/wypt/meta_database.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from .database import Database
+from .paste_database import PasteDatabase
 
 
 class MetaDatabase(Database):
@@ -9,3 +10,22 @@ class MetaDatabase(Database):
 
     sql_file: str = "tables/meta_database_tbl.sql"
     table_name: str = "pastemeta"
+
+    def get_keys_to_fetch(self, limit: int = 25) -> tuple[str, ...]:
+        """
+        Return `limit` of paste key values that have not been fetched.
+
+        NOTE: Can return less than `limit` or empty results.
+        """
+
+        sql = (
+            f"SELECT a.key FROM {self.table_name} a "
+            f"LEFT JOIN {PasteDatabase.table_name} b "
+            f"ON a.key = b.key WHERE b.key IS NULL LIMIT ?"
+        )
+
+        with self.cursor() as cursor:
+            cursor.execute(sql, (limit,))
+            results = cursor.fetchall()
+
+        return tuple(r[0] for r in results)
--- tests/meta_database_test.py
+++ tests/meta_database_test.py
@@ -1,6 +1,32 @@
 from __future__ import annotations
 
+import json
 from pathlib import Path
+from sqlite3 import Connection
+
+from wypt.meta_database import MetaDatabase
+from wypt.model import Paste
+from wypt.model import PasteMeta
+from wypt.paste_database import PasteDatabase
 
 
 METAS = Path("tests/fixture/scrape_resp.json").read_text()
+
+
+def test_get_keys_to_fetch() -> None:
+    # Overly complex setup joyness
+    # Setup two databases with mock data. Results should expect
+    # all keys of meta fixture to be returns sans 0th index key.
+    conn = Connection(":memory:")
+    pastedb = PasteDatabase(conn)
+    metadb = MetaDatabase(conn)
+    metas = [PasteMeta(**meta) for meta in json.loads(METAS)]
+    paste = Paste(metas[0].key, "", "")
+    metadb.insert_many(metas)
+    pastedb.insert(paste)
+
+    results = metadb.get_keys_to_fetch()
+    print(results)
+
+    assert metas[0].key not in results
+    assert len(results) == len(metas) - 1
