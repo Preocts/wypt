de9f6524b946fed94e753d24a336762f6d3f8edc
Preocts <preocts@gmail.com>
2022-09-10 21:11:20 -0400

    Move database classes into database.py

--- src/wypt/view.py
+++ src/wypt/view.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from sqlite3 import Connection
 
-from .match_database import MatchDatabase
+from .database import MatchDatabase
 
 if __name__ == "__main__":
     dbconn = Connection("wypt_db.sqlite3")
