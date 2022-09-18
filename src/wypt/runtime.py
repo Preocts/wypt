91930c91189be3be28a6c499ec21a519b9477443
Preocts <preocts@gmail.com>
2022-09-18 15:53:05 -0400

    Add database connection and getter

--- src/wypt/runtime.py
+++ src/wypt/runtime.py
@@ -4,9 +4,15 @@ from __future__ import annotations
 import logging
 from dataclasses import dataclass
 from pathlib import Path
+from sqlite3 import Connection
 from typing import Any
 
 import tomli
+
+from .database import Database
+from .model import Match
+from .model import Meta
+from .model import Paste
 
 
 @dataclass(frozen=True)
@@ -24,26 +30,44 @@ class Runtime:
     def __init__(self) -> None:
         """Provide runtime setup utility."""
         self._config: _Config | None = None
+        self._database: Database | None = None
 
-    @property
-    def config(self) -> _Config:
+    def get_config(self) -> _Config:
         """Return loaded config, will load default if not already loaded."""
         if self._config is None:
             self._config = self.load_config()
         return self._config
 
+    def get_database(self) -> Database:
+        """Return connected database, will connect in-memory if not connected."""
+        if self._database is None:
+            self._database = self.connect_database()
+        return self._database
+
+    def connect_database(self, database_file: str = ":memory:") -> Database:
+        """Connect to sqlite3 database. Uses in-memory location by default."""
+        if self._database is None:
+            # Connect to and build database
+            dbconn = Connection(self.get_config().database_file)
+            self._database = Database(dbconn)
+            self._database.add_table("paste", "tables/paste_database_tbl.sql", Paste)
+            self._database.add_table("meta", "tables/meta_database_tbl.sql", Meta)
+            self._database.add_table("match", "tables/match_database_tbl.sql", Match)
+        return self._database
+
     def load_config(self, config_file: str = "wypt.toml") -> _Config:
-        """Load config and set logging. Uses defaults if not found."""
+        """Load and return config file. Uses defaults if not found."""
         _config_file = Path(config_file)
         config: dict[str, Any] = {}
         if _config_file.exists():
             config = tomli.load(_config_file.open("rb")).get("CONFIG") or {}
-        return _Config(**config) if config else _Config()
+        self._config = _Config(**config) if config else _Config()
+        return self._config
 
     def set_logging(self) -> None:
         """Set root logging with defined format."""
         # Purposely use basicConfig to avoid mucking with loggers already set.
         logging.basicConfig(
-            level=self.config.logging_level,
-            format=self.config.logging_format,
+            level=self.get_config().logging_level,
+            format=self.get_config().logging_format,
         )
--- tests/runtime_time_test.py
+++ tests/runtime_time_test.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from wypt.database import Database
 from wypt.runtime import _Config
 from wypt.runtime import Runtime
 
@@ -9,6 +10,7 @@ def test_load_config() -> None:
 
     config = runtime.load_config("tests/fixture/wypt.toml")
 
+    assert config is runtime._config
     assert config.logging_level == "INFO"
     assert config.retain_posts_for_days == 99
 
@@ -21,8 +23,26 @@ def test_load_missing_config() -> None:
     assert default == _Config()
 
 
-def test_set_logging() -> None:
+def test_get_config_returns_cached_copy_of_config() -> None:
+    runtime = Runtime()
+
+    config = runtime.get_config()
+    config_too = runtime.get_config()
+
+    assert config is config_too
+
+
+def test_set_logging_tests_nothing_but_here_we_are() -> None:
     # Coverage goes brrr
     runtime = Runtime()
 
     runtime.set_logging()
+
+
+def test_connect_database() -> None:
+    runtime = Runtime()
+
+    result = runtime.connect_database()
+
+    assert isinstance(result, Database)
+    assert result is runtime._database
