7d924b5e2ec37f3b6814c7538867384ab287d342
Preocts <preocts@gmail.com>
2022-09-21 19:04:58 -0400

    Clean up cli with new runtime handler

--- src/wypt/cli.py
+++ src/wypt/cli.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 from .paste_scanner import PasteScanner
-from .pastebin_api import PastebinAPI
 from .runtime import Runtime
 
 runtime = Runtime()
@@ -12,21 +11,12 @@ runtime.set_logging()
 
 def scan() -> int:
     """Point of entry for paste scanning."""
-    # Connect to and build database
     config = runtime.get_config()
-    database = runtime.connect_database(config.database_file)
 
-    # Load pattern file
-    pattern_config = runtime.get_patterns()
-
-    # Create API client
-    api = PastebinAPI()
-
-    # Create controller
     gatherer = PasteScanner(
-        database=database,
-        patterns=pattern_config,
-        pastebin_api=api,
+        database=runtime.connect_database(config.database_file),
+        patterns=runtime.get_patterns(),
+        pastebin_api=runtime.get_api(),
         save_paste_content=True,
     )
 
