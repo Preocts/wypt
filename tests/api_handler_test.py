f7fd0a3c9f2dede87c97220654d1f584be57aba8
Preocts <preocts@preocts.com>
2024-01-22 22:08:10 -0500

    Simple pagination links for table

--- tests/api_handler_test.py
+++ tests/api_handler_test.py
@@ -42,3 +42,36 @@ def test_delete_rows(handler: APIHandler) -> None:
 def test_clean_split(text: str, expected: list[str]) -> None:
     result = APIHandler._clean_split(text)
     assert result == expected
+
+
+def test_get_matchview_has_results(handler: APIHandler) -> None:
+    results = handler.get_matchview()
+
+    assert results
+
+
+def test_Get_matchview_has_no_results(handler: APIHandler) -> None:
+    results = handler.get_matchview(0, 0)
+
+    assert not results
+
+
+def test_get_matchview_links_defaults(handler: APIHandler) -> None:
+    previous_page, next_page = handler.get_matchview_params()
+
+    assert previous_page == ""
+    assert next_page == ""
+
+
+def test_get_matchview_links_has_next(handler: APIHandler) -> None:
+    previous_page, next_page = handler.get_matchview_params(1, 0)
+
+    assert previous_page == ""
+    assert next_page == "limit=1&offset=1"
+
+
+def test_get_matchview_links_has_previous(handler: APIHandler) -> None:
+    previous_page, next_page = handler.get_matchview_params(1, 1)
+
+    assert previous_page == "limit=1&offset=0"
+    assert next_page == ""
