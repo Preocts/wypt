145bff3202518bcd1538dd91a33e7d5a306db764
Preocts <preocts@preocts.com>
2024-01-24 23:07:22 -0500

    Call table partial after loading match table view

--- src/wypt/api.py
+++ src/wypt/api.py
@@ -46,14 +46,7 @@ def gridsample(request: Request) -> HTMLResponse:
 @routes.get("/matchview")
 def matchview_main(request: Request, limit: int = 100, offset: int = 0) -> HTMLResponse:
     """Main view for MatchView model."""
-    previous_params, next_params = api_handler.get_matchview_params(limit, offset)
-    context = {
-        "previous_params": previous_params,
-        "next_params": next_params,
-        "matchviews": api_handler.get_matchview(limit, offset),
-    }
-
-    return template.TemplateResponse(request, "matchview/index.html", context)
+    return template.TemplateResponse(request, "matchview/index.html")
 
 
 @routes.get("/matchviewtable")
